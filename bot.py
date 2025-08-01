9aba3cfd5a174d0eeadf2f6549e1b84b2ed5ab23
Agnav <ashiagnav1@gmail.com>
2025-08-01 11:14:32 +0530

    latest commit

--- bot.py
+++ bot.py
@@ -142,7 +142,7 @@ def main():
     app.add_handler(CommandHandler("qr", send))
 
     scheduler = AsyncIOScheduler(event_loop=loop, timezone="Asia/Kolkata")
-    scheduler.add_job(scheduled_job, "cron", hour=11, minute=10)
+    scheduler.add_job(scheduled_job, "cron", hour=6, minute=00)
     scheduler.start()
 
 
